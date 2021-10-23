5a09db2bc3e3e95e03167193e1acf4ec4c64ccee
FelipeOtz <felipeht25@gmail.com>
2021-10-22 21:40:42 -0300

    Update jogo.java

--- src/jogo.java
+++ src/jogo.java
@@ -3,22 +3,6 @@ public class jogo {
 
 	public static void main(String[] args) {
 
-		System.out.println("Teste 1 felipe");
-
-		System.out.println("Felipe");
-
-		System.out.println("MELQUI VIEIRA");
-		System.out.println("Teste 1171");
-
-		System.out.println("Roberto aloo");
-		System.out.println("O Senhor da Confusão");
-
-		System.out.println("Julia juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuubsz");
-
-		System.out.println("Rafael");
-
-		System.out.println("Teste github");
-
 	}
 
 }
