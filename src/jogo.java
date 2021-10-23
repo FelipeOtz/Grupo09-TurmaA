bd1deb11d2d984d2fe9cc38847fed26bce6442dd
Julia <julia.amaralsantoss@gmail.com>
2021-10-22 21:43:53 -0300

    Menu Update
    Menu RPG

--- src/jogo.java
+++ src/jogo.java
@@ -3,6 +3,51 @@ public class jogo {
 
 	public static void main(String[] args) {
 
+		Scanner entrada = new Scanner(System.in);
+
+        int numeroMenu;
+
+        boolean rodar = true;
+        
+        System.out.println("Escolha uma das opções abaixo:\n\n"
+                            +"1- Instruções\n"
+                            +"2- Jogar\n"
+                            +"3- Créditos\n"
+                            +"4- Sair");
+
+        do {
+
+            numeroMenu = entrada.nextInt();
+
+            switch (numeroMenu) {
+
+            case 1:
+                System.out.println("Aguardando instruções...");
+                break;
+
+            case 2:
+                System.out.println("Aguardando o jogo iniciar...");
+                break;
+
+            case 3:
+                System.out.println("Aguardando créditos...");
+                break;
+
+            case 4:
+                System.out.println("Encerrando o menu... até mais!");
+                rodar = false;
+                break;
+
+            default:
+                System.out.println("Valor inválido.");
+                break;
+            }
+
+        } while (rodar);
+
+        entrada.close();
+    }
+	
 	}
 
 }
