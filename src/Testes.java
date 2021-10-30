ba3d4f8178ad6319d22f3cb78e40d00494b0eba3
RobertoJSJTV <91293740+RobertoJSJTV@users.noreply.github.com>
2021-10-29 22:11:06 -0300

    Update Testes.java

--- src/Testes.java
+++ src/Testes.java
@@ -1,4 +1,18 @@
 
 public class Testes {
-	
+	  public static void main(String[] args) {
+		    boolean rosto;
+		    String nome = "Trinity";
+		    /* DICA: UMA VARIÁVEL BOOEANA SE INICA COM VALOR FALSO*/
+
+		    System.out.println("System.out._ln(" + "Meu Nome é + nome" + ");\nrosto = __" + ";");
+
+		    System.out.println("Qual o comando faltante na primeira linha: ");
+
+		    System.out.println("Qual o comando faltante na segunda linha: ");
+
+
+
+
+		    }
 }
