08c71253faa84411426ae1817718dbd0412ac30f
FelipeOtz <felipeht25@gmail.com>
2021-10-30 22:26:31 -0300

    teste commit no eclipse

--- src/Testes.java
+++ src/Testes.java
@@ -1,33 +1,24 @@
 
 public class Testes {
-	  public static void main(String[] args) {
-		    boolean rosto;
-		    String nome = "Trinity";
-		    /* DICA: UMA VARIÁVEL BOOEANA SE INICA COM VALOR FALSO*/
+	public static void main(String[] args) {
+		boolean rosto;
+		String nome = "Trinity";
+		/* DICA: UMA VARIÁVEL BOOEANA SE INICA COM VALOR FALSO */
 
-		    System.out.println("System.out._ln(" + "Meu Nome é + nome" + ");\nrosto = __" + ";");
+		System.out.println("teste");
 
-		    System.out.println("Qual o comando faltante na primeira linha: ");
+		System.out.println("System.out._ln(" + "Meu Nome é + nome" + ");\nrosto = __" + ";");
 
-		    System.out.println("Qual o comando faltante na segunda linha:                              __\r\n"
-		    		+ "                     /\\    .-\" /\r\n"
-		    		+ "                    /  ; .'  .' \r\n"
-		    		+ "                   :   :/  .'   \r\n"
-		    		+ "                    \\  ;-.'     \r\n"
-		    		+ "       .--\"\"\"\"--..__/     `.    \r\n"
-		    		+ "     .'           .'    `o  \\   \r\n"
-		    		+ "    /                    `   ;  \r\n"
-		    		+ "   :                  \\      :  \r\n"
-		    		+ " .-;        -.         `.__.-'  \r\n"
-		    		+ ":  ;          \\     ,   ;       \r\n"
-		    		+ "'._:           ;   :   (        \r\n"
-		    		+ "    \\/  .__    ;    \\   `-.     \r\n"
-		    		+ " bug ;     \"-,/_..--\"`-..__)    \r\n"
-		    		+ "     '\"\"--.._:\r\n"
-		    		+ "");
+		System.out.println("Qual o comando faltante na primeira linha: ");
 
+		System.out.println("Qual o comando faltante na segunda linha:                              __\r\n"
+				+ "                     /\\    .-\" /\r\n" + "                    /  ; .'  .' \r\n"
+				+ "                   :   :/  .'   \r\n" + "                    \\  ;-.'     \r\n"
+				+ "       .--\"\"\"\"--..__/     `.    \r\n" + "     .'           .'    `o  \\   \r\n"
+				+ "    /                    `   ;  \r\n" + "   :                  \\      :  \r\n"
+				+ " .-;        -.         `.__.-'  \r\n" + ":  ;          \\     ,   ;       \r\n"
+				+ "'._:           ;   :   (        \r\n" + "    \\/  .__    ;    \\   `-.     \r\n"
+				+ " bug ;     \"-,/_..--\"`-..__)    \r\n" + "     '\"\"--.._:\r\n" + "");
 
-
-
-		    }
+	}
 }
